88d7dc5e66b16862c1acd3ed561d52a375bc1b7b
kKaskak <117831817+kKaskak@users.noreply.github.com>
2023-08-05 16:46:35 +0300

    class name change fix

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="custom-loader__container">
-      <div class="custom-loader"></div>
+      <div className="custom-loader"></div>
     </div>
   );
 };
